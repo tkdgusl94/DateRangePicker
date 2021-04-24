f8f513ede526204534f469746a772c731cfed16b
SangHyun Jeong <tkdgusl94@gmail.com>
2021-04-24 14:00:12 +0900

    init project

---
+++ daterangepicker/src/main/java/com/leveloper/daterangepicker/LvDateRangePicker.kt
@@ -0,0 +1,12 @@
+package com.leveloper.daterangepicker
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+
+class LvDateRangePicker @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int
+): RecyclerView(context, attrs, defStyle) {
+}
